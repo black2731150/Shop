a6162c875d2ee412e3cc20f31ed4defcb236f349
xiaoqi <1737429432@qq.com>
2023-07-04 17:46:28 +0800

    加上account

---
+++ account/internal/data/user.go
@@ -0,0 +1,49 @@
+package data
+
+import (
+	"account/internal/biz"
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"gorm.io/gorm"
+)
+
+type userRepo struct {
+	data  *Data
+	log   *log.Helper
+	table *gorm.DB
+}
+
+func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
+	return &userRepo{
+		data:  data,
+		log:   log.NewHelper(logger),
+		table: data.db.Table("user"),
+	}
+}
+
+func (u *userRepo) FetchByUsername(ctx context.Context, username string) (user *biz.User, err error) {
+	user = &biz.User{}
+	u.table.WithContext(ctx).First(user, "username = ?", username)
+	if user.ID == 0 {
+		return nil, biz.ErrUserNotFound
+	}
+	return user, nil
+}
+
+func (u *userRepo) FetchByEmail(ctx context.Context, email string) (user *biz.User, err error) {
+	user = &biz.User{}
+	u.table.WithContext(ctx).First(user, "email = ?", email)
+	if user.ID == 0 {
+		return nil, biz.ErrUserNotFound
+	}
+	return user, nil
+}
+
+func (u *userRepo) Save(ctx context.Context, user *biz.User) (id int64, err error) {
+	result := u.table.WithContext(ctx).Create(user)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+	return user.ID, nil
+}
---
+++ account/internal/service/account.go
@@ -0,0 +1,96 @@
+package service
+
+import (
+	v1 "account/api/account/v1"
+	"account/internal/biz"
+	"context"
+
+	"github.com/go-kratos/kratos/v2/errors"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type accountService struct {
+	v1.UnimplementedAccountServer
+	log *log.Helper
+	auc *biz.AccountUseCase
+}
+
+func NewAccountUseCase(logger log.Logger, auc *biz.AccountUseCase) v1.AccountServer {
+	return &accountService{
+		log: log.NewHelper(logger),
+		auc: auc,
+	}
+}
+
+func (a *accountService) Register(ctx context.Context, request *v1.RegisterRequest) (*v1.RegisterResponse, error) {
+	err := a.auc.Register(ctx, request.GetUsername(), request.GetPassword(), request.GetEmail())
+	if err != nil {
+		return nil, errors.New(500, "注册失败", err.Error())
+	}
+	return &v1.RegisterResponse{
+		Success: true,
+	}, nil
+}
+
+func (a *accountService) TestEmail(ctx context.Context, request *v1.TestEmailRequest) (*v1.TestEmailResponse, error) {
+	err := a.auc.TestEmail(ctx, request.GetEmail())
+	if err != nil {
+		return nil, errors.New(500, "邮箱验证失败", err.Error())
+	}
+	return &v1.TestEmailResponse{
+		Success: true,
+	}, nil
+}
+
+func (a *accountService) SendEmailCode(ctx context.Context, request *v1.SendEmailCodeRequest) (*v1.SendEmailCodeResponse, error) {
+	err := a.auc.SendEmailCode(ctx, request.GetEmail())
+	if err != nil {
+		return nil, errors.New(500, "发送验证码失败", err.Error())
+	}
+	return &v1.SendEmailCodeResponse{
+		Success: true,
+	}, nil
+}
+
+func (a *accountService) TestEmailCode(ctx context.Context, request *v1.TestEmailCodeRequest) (*v1.TestEmailCodeResponse, error) {
+	err := a.auc.TestEmailCode(ctx, request.GetEmail(), request.GetEmailCode())
+	if err != nil {
+		return nil, errors.New(500, "验证码不正确", err.Error())
+	}
+	return &v1.TestEmailCodeResponse{
+		Success: true,
+	}, nil
+}
+
+func (a *accountService) TestUsername(ctx context.Context, request *v1.TestUsernameRequest) (*v1.TestUsernameResponse, error) {
+	err := a.auc.TestUsername(ctx, request.GetUsername())
+	if err != nil {
+		return nil, errors.New(500, "用户名不符合规范", err.Error())
+	}
+	return &v1.TestUsernameResponse{
+		Success: true,
+	}, nil
+}
+
+func (a *accountService) LoginWithPassword(ctx context.Context, request *v1.LoginWithPasswordRequest) (*v1.LoginWithPasswordResponse, error) {
+	token, err := a.auc.LoginWithPassword(ctx, request.GetUsernameOrEmail(), request.Password)
+	if err != nil {
+		return nil, errors.New(500, "登录失败", err.Error())
+	}
+	return &v1.LoginWithPasswordResponse{
+		Success: true,
+		Token:   token,
+	}, nil
+}
+
+func (a *accountService) LoginWithCode(ctx context.Context, request *v1.LoginWithCodeRequest) (*v1.LoginWithCodeRsponse, error) {
+	token, err := a.auc.LoginWithCode(ctx, request.Email, request.EmailCode)
+	if err != nil {
+		return nil, errors.New(500, "登录失败", err.Error())
+	}
+	return &v1.LoginWithCodeRsponse{
+		Success: true,
+		Token:   token,
+	}, nil
+}
